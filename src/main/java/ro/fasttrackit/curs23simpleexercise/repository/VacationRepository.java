3b8514d196b86b908a8d34769273bee58ca0a15c
Varadi <mark.varadi03@gmail.com>
2020-05-15 11:11:53 +0300

    curs 23 with criteria builder implemented

--- src/main/java/ro/fasttrackit/curs23simpleexercise/repository/VacationRepository.java
+++ src/main/java/ro/fasttrackit/curs23simpleexercise/repository/VacationRepository.java
@@ -3,11 +3,5 @@ package ro.fasttrackit.curs23simpleexercise.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ro.fasttrackit.curs23simpleexercise.domain.Vacation;
 
-import java.util.List;
-
 public interface VacationRepository extends JpaRepository<Vacation, Integer> {
-
-    List<Vacation> findVacationsByLocationIgnoreCase(String location);
-
-    List<Vacation> findVacationsByPriceLessThanEqual(Integer maxPrice);
 }
